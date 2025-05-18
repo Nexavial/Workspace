bff0ec725cf243ced0149506bc7c3d3d2a5af3b5
Nexavial <nexavialhub@gmail.com>
2025-05-18 08:22:25 +0700

    Update loaddata.lua

--- Anime Rangers X/loaddata.lua
+++ Anime Rangers X/loaddata.lua
@@ -1,1 +1,255 @@
+local Data = {
+    Code = {},
+    World = {
+        Story = {
+            OnePiece = {
+                Size = {
+                    X = 1024,
+                    Y = 523
+                },
+                Name = "Voocha Village",
+                Image = "rbxassetid://139956759761186",
+                LayoutOrder = 1
+            },
+            Namek = {
+                Size = {
+                    X = 1024,
+                    Y = 523
+                },
+                Name = "Green Planet",
+                Image = "rbxassetid://83551345782867",
+                LayoutOrder = 2
+            },
+            DemonSlayer = {
+                Size = {
+                    X = 1024,
+                    Y = 523
+                },
+                Name = "Demon Forest",
+                Image = "rbxassetid://88600116685738",
+                LayoutOrder = 3
+            },
+            Naruto = {
+                Size = {
+                    X = 1024,
+                    Y = 523
+                },
+                Name = "Leaf Village",
+                Image = "rbxassetid://93900824627541",
+                LayoutOrder = 4
+            },
+            OPM = {
+                Size = {
+                    X = 1024,
+                    Y = 523
+                },
+                Name = "Z City",
+                Image = "rbxassetid://116889089754097",
+                LayoutOrder = 5
+            },
+            TokyoGhoul = {
+                Size = {
+                    X = 1024,
+                    Y = 523
+                },
+                Name = "Ghoul City",
+                Image = "rbxassetid://104837674477520",
+                LayoutOrder = 6
+            }
+        },
+        Challenge = {
+            "High Cost",
+            "Speed Demon",
+            "Budget Cuts",
+            "One Life Only",
+            "Limited Towers",
+            "Expensive Upgrades"
+        }
+    },
+    Image = {
+        Webhook = {
+            OPM = "https://media.discordapp.net/attachments/1367832741508415518/1367833303503212634/Nexavial_Z_City.png?ex=68160588&is=6814b408&hm=4970928ca8010d09378a4a2c690e031f0cd1b5910dbba6605d5cc3f731990941&=&format=webp&quality=lossless",
+            Game = "https://media.discordapp.net/attachments/1367832741508415518/1367836128987582535/Nexavial_ARX.png?ex=6816082a&is=6814b6aa&hm=78d2028287522da88e22b1cb33f35cdc8220168957d4e74de5c9f7b760a3f1e5&=&format=webp&quality=lossless",
+            Namek = "https://media.discordapp.net/attachments/1367832741508415518/1367833302387527831/Nexavial_Green_Planet.png?ex=68160588&is=6814b408&hm=95a5a9af058c50df5c7b0c49c0f9fa4ad3e418808010c62c44ced31f013bf3a3&=&format=webp&quality=lossless",
+            Naruto = "https://media.discordapp.net/attachments/1367832741508415518/1367833304073768990/Nexavial_Leaf_Village.png?ex=68160588&is=6814b408&hm=ae7be24ccacb455a6b2c0eda8aecb55bd82b72a38899e10c436aa9da99f2a083&=&format=webp&quality=lossless",
+            OnePiece = "https://media.discordapp.net/attachments/1367832741508415518/1367833302035337257/Nexavial_Voocha_Village.png?ex=68160588&is=6814b408&hm=b4c877dea9f3d2b4770f221e2913c55a67fb97c9ee7aa474716aef5ca4a21376&=&format=webp&quality=lossless",
+            EasterEgg = "https://media.discordapp.net/attachments/1367832741508415518/1367834629909905458/Nexavial_Easter_Event.png?ex=681606c4&is=6814b544&hm=12bae9e62756080258face4795332a4b70df26367fe20c0715f2197c0737a61d&=&format=webp&quality=lossless&width=550&height=293",
+            TokyoGhoul = "https://media.discordapp.net/attachments/1367832741508415518/1373466347207655514/Nexavial_Ghoul_City.png?ex=682a83b6&is=68293236&hm=25ab2222c1fac2b0c410de2daffc977c85f0d8d881c9790886fe6166e8f6e293&=&format=webp&quality=lossless",
+            DemonSlayer = "https://media.discordapp.net/attachments/1367832741508415518/1367833302886518805/Nexavial_Demon_Forest.png?ex=68160588&is=6814b408&hm=486f4bfecacf30f9459679d86a71474ace417c218649a7a4f41204ec2d3dcab3&=&format=webp&quality=lossless"
+        }
+    },
+    Next_Levels = {
+        OnePiece_Chapter1 = "OnePiece_Chapter2",
+        OnePiece_Chapter2 = "OnePiece_Chapter3",
+        OnePiece_Chapter3 = "OnePiece_Chapter4",
+        OnePiece_Chapter4 = "OnePiece_Chapter5",
+        OnePiece_Chapter5 = "OnePiece_Chapter6",
+        OnePiece_Chapter6 = "OnePiece_Chapter7",
+        OnePiece_Chapter7 = "OnePiece_Chapter8",
+        OnePiece_Chapter8 = "OnePiece_Chapter9",
+        OnePiece_Chapter9 = "OnePiece_Chapter10",
+        OnePiece_Chapter10 = "Namek_Chapter1",
+        OnePiece_RangerStage1 = "OnePiece_RangerStage2",
+        OnePiece_RangerStage2 = "OnePiece_RangerStage3",
+        OnePiece_RangerStage3 = "Namek_RangerStage1",
 
+        Namek_Chapter1 = "Namek_Chapter2",
+        Namek_Chapter2 = "Namek_Chapter3",
+        Namek_Chapter3 = "Namek_Chapter4",
+        Namek_Chapter4 = "Namek_Chapter5",
+        Namek_Chapter5 = "Namek_Chapter6",
+        Namek_Chapter6 = "Namek_Chapter7",
+        Namek_Chapter7 = "Namek_Chapter8",
+        Namek_Chapter8 = "Namek_Chapter9",
+        Namek_Chapter9 = "Namek_Chapter10",
+        Namek_Chapter10 = "DemonSlayer_Chapter1",
+        Namek_RangerStage1 = "Namek_RangerStage2",
+        Namek_RangerStage2 = "Namek_RangerStage3",
+        Namek_RangerStage3 = "DemonSlayer_RangerStage1",
+
+        DemonSlayer_Chapter1 = "DemonSlayer_Chapter2",
+        DemonSlayer_Chapter2 = "DemonSlayer_Chapter3",
+        DemonSlayer_Chapter3 = "DemonSlayer_Chapter4",
+        DemonSlayer_Chapter4 = "DemonSlayer_Chapter5",
+        DemonSlayer_Chapter5 = "DemonSlayer_Chapter6",
+        DemonSlayer_Chapter6 = "DemonSlayer_Chapter7",
+        DemonSlayer_Chapter7 = "DemonSlayer_Chapter8",
+        DemonSlayer_Chapter8 = "DemonSlayer_Chapter9",
+        DemonSlayer_Chapter9 = "DemonSlayer_Chapter10",
+        DemonSlayer_Chapter10 = "Naruto_Chapter1",
+        DemonSlayer_RangerStage1 = "DemonSlayer_RangerStage2",
+        DemonSlayer_RangerStage2 = "DemonSlayer_RangerStage3",
+        DemonSlayer_RangerStage3 = "Naruto_RangerStage1",
+
+        Naruto_Chapter1 = "Naruto_Chapter2",
+        Naruto_Chapter2 = "Naruto_Chapter3",
+        Naruto_Chapter3 = "Naruto_Chapter4",
+        Naruto_Chapter4 = "Naruto_Chapter5",
+        Naruto_Chapter5 = "Naruto_Chapter6",
+        Naruto_Chapter6 = "Naruto_Chapter7",
+        Naruto_Chapter7 = "Naruto_Chapter8",
+        Naruto_Chapter8 = "Naruto_Chapter9",
+        Naruto_Chapter9 = "Naruto_Chapter10",
+        Naruto_Chapter10 = "OPM_Chapter1",
+        Naruto_RangerStage1 = "Naruto_RangerStage2",
+        Naruto_RangerStage2 = "Naruto_RangerStage3",
+        Naruto_RangerStage3 = "OPM_RangerStage1",
+
+        OPM_Chapter1 = "OPM_Chapter2",
+        OPM_Chapter2 = "OPM_Chapter3",
+        OPM_Chapter3 = "OPM_Chapter4",
+        OPM_Chapter4 = "OPM_Chapter5",
+        OPM_Chapter5 = "OPM_Chapter6",
+        OPM_Chapter6 = "OPM_Chapter7",
+        OPM_Chapter7 = "OPM_Chapter8",
+        OPM_Chapter8 = "OPM_Chapter9",
+        OPM_Chapter9 = "OPM_Chapter10",
+        OPM_Chapter10 = "TokyoGhoul_Chapter1",
+        OPM_RangerStage1 = "OPM_RangerStage2",
+        OPM_RangerStage2 = "OPM_RangerStage3",
+        OPM_RangerStage3 = "TokyoGhoul_RangerStage1",
+
+        TokyoGhoul_Chapter1 = "TokyoGhoul_Chapter2",
+        TokyoGhoul_Chapter2 = "TokyoGhoul_Chapter3",
+        TokyoGhoul_Chapter3 = "TokyoGhoul_Chapter4",
+        TokyoGhoul_Chapter4 = "TokyoGhoul_Chapter5",
+        TokyoGhoul_Chapter5 = "TokyoGhoul_Chapter6",
+        TokyoGhoul_Chapter6 = "TokyoGhoul_Chapter7",
+        TokyoGhoul_Chapter7 = "TokyoGhoul_Chapter8",
+        TokyoGhoul_Chapter8 = "TokyoGhoul_Chapter9",
+        TokyoGhoul_Chapter9 = "TokyoGhoul_Chapter10",
+        TokyoGhoul_Chapter10 = nil,
+        TokyoGhoul_RangerStage1 = "TokyoGhoul_RangerStage2",
+        TokyoGhoul_RangerStage2 = "TokyoGhoul_RangerStage3",
+        TokyoGhoul_RangerStage3 = "TokyoGhoul_RangerStage4",
+        TokyoGhoul_RangerStage4 = "TokyoGhoul_RangerStage5",
+        TokyoGhoul_RangerStage5 = nil
+    },
+    Required_Levels = {
+        OnePiece_Chapter1 = true,
+        OnePiece_Chapter2 = "OnePiece_Chapter1",
+        OnePiece_Chapter3 = "OnePiece_Chapter2",
+        OnePiece_Chapter4 = "OnePiece_Chapter3",
+        OnePiece_Chapter5 = "OnePiece_Chapter4",
+        OnePiece_Chapter6 = "OnePiece_Chapter5",
+        OnePiece_Chapter7 = "OnePiece_Chapter6",
+        OnePiece_Chapter8 = "OnePiece_Chapter7",
+        OnePiece_Chapter9 = "OnePiece_Chapter8",
+        OnePiece_Chapter10 = "OnePiece_Chapter9",
+        OnePiece_RangerStage1 = true,
+        OnePiece_RangerStage2 = "OnePiece_Chapter10",
+        OnePiece_RangerStage3 = "OnePiece_Chapter10",
+
+        Namek_Chapter1 = "OnePiece_Chapter10",
+        Namek_Chapter2 = "Namek_Chapter1",
+        Namek_Chapter3 = "Namek_Chapter2",
+        Namek_Chapter4 = "Namek_Chapter3",
+        Namek_Chapter5 = "Namek_Chapter4",
+        Namek_Chapter6 = "Namek_Chapter5",
+        Namek_Chapter7 = "Namek_Chapter6",
+        Namek_Chapter8 = "Namek_Chapter7",
+        Namek_Chapter9 = "Namek_Chapter8",
+        Namek_Chapter10 = "Namek_Chapter9",
+        Namek_RangerStage1 = "Namek_Chapter10",
+        Namek_RangerStage2 = "Namek_Chapter10",
+        Namek_RangerStage3 = "Namek_Chapter10",
+
+        DemonSlayer_Chapter1 = "Namek_Chapter10",
+        DemonSlayer_Chapter2 = "DemonSlayer_Chapter1",
+        DemonSlayer_Chapter3 = "DemonSlayer_Chapter2",
+        DemonSlayer_Chapter4 = "DemonSlayer_Chapter3",
+        DemonSlayer_Chapter5 = "DemonSlayer_Chapter4",
+        DemonSlayer_Chapter6 = "DemonSlayer_Chapter5",
+        DemonSlayer_Chapter7 = "DemonSlayer_Chapter6",
+        DemonSlayer_Chapter8 = "DemonSlayer_Chapter7",
+        DemonSlayer_Chapter9 = "DemonSlayer_Chapter8",
+        DemonSlayer_Chapter10 = "DemonSlayer_Chapter9",
+        DemonSlayer_RangerStage1 = "DemonSlayer_Chapter10",
+        DemonSlayer_RangerStage2 = "DemonSlayer_Chapter10",
+        DemonSlayer_RangerStage3 = "DemonSlayer_Chapter10",
+
+        Naruto_Chapter1 = "DemonSlayer_Chapter10",
+        Naruto_Chapter2 = "Naruto_Chapter1",
+        Naruto_Chapter3 = "Naruto_Chapter2",
+        Naruto_Chapter4 = "Naruto_Chapter3",
+        Naruto_Chapter5 = "Naruto_Chapter4",
+        Naruto_Chapter6 = "Naruto_Chapter5",
+        Naruto_Chapter7 = "Naruto_Chapter6",
+        Naruto_Chapter8 = "Naruto_Chapter7",
+        Naruto_Chapter9 = "Naruto_Chapter8",
+        Naruto_Chapter10 = "Naruto_Chapter9",
+        Naruto_RangerStage1 = "Naruto_Chapter10",
+        Naruto_RangerStage2 = "Naruto_Chapter10",
+        Naruto_RangerStage3 = "Naruto_Chapter10",
+
+        OPM_Chapter1 = "Naruto_Chapter10",
+        OPM_Chapter2 = "OPM_Chapter1",
+        OPM_Chapter3 = "OPM_Chapter2",
+        OPM_Chapter4 = "OPM_Chapter3",
+        OPM_Chapter5 = "OPM_Chapter4",
+        OPM_Chapter6 = "OPM_Chapter5",
+        OPM_Chapter7 = "OPM_Chapter6",
+        OPM_Chapter8 = "OPM_Chapter7",
+        OPM_Chapter9 = "OPM_Chapter8",
+        OPM_Chapter10 = "OPM_Chapter9",
+        OPM_RangerStage1 = "OPM_Chapter10",
+        OPM_RangerStage2 = "OPM_Chapter10",
+        OPM_RangerStage3 = "OPM_Chapter10",
+
+        TokyoGhoul_Chapter1 = "OPM_Chapter10",
+        TokyoGhoul_Chapter2 = "TokyoGhoul_Chapter1",
+        TokyoGhoul_Chapter3 = "TokyoGhoul_Chapter2",
+        TokyoGhoul_Chapter4 = "TokyoGhoul_Chapter3",
+        TokyoGhoul_Chapter5 = "TokyoGhoul_Chapter4",
+        TokyoGhoul_Chapter6 = "TokyoGhoul_Chapter5",
+        TokyoGhoul_Chapter7 = "TokyoGhoul_Chapter6",
+        TokyoGhoul_Chapter8 = "TokyoGhoul_Chapter7",
+        TokyoGhoul_Chapter9 = "TokyoGhoul_Chapter8",
+        TokyoGhoul_Chapter10 = "TokyoGhoul_Chapter9",
+        TokyoGhoul_RangerStage1 = "TokyoGhoul_Chapter10",
+        TokyoGhoul_RangerStage2 = "TokyoGhoul_Chapter10",
+        TokyoGhoul_RangerStage3 = "TokyoGhoul_Chapter10",
+        TokyoGhoul_RangerStage4 = "TokyoGhoul_Chapter10",
+        TokyoGhoul_RangerStage5 = "TokyoGhoul_Chapter10"
+    }
+};Data.__index = Data;return Data
